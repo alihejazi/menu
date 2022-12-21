f5d018e070036512998ab3d63497d907b888d027
Ali Hejazi <ahejazi1999@gmail.com>
2022-12-21 14:54:47 +0000

    Removed comments

--- script.js
+++ script.js
@@ -12,28 +12,7 @@ menuCloseBtn.onclick = function() {
 navLinks.style.left = "-100%";
 }
 
-
-// sidebar submenu open close js code
-// let htmlcssArrow = document.querySelector(".match-funding-arrow");
-// htmlcssArrow.onclick = function() {
-//  navLinks.classList.toggle("show-match-funding-sub-menu");
-// }
-// let moreArrow = document.querySelector(".match-funding-opportunities-sub-sub-menu-arrow");
-// moreArrow.onclick = function() {
-//  navLinks.classList.toggle("show-match-funding-opportunities-sub-sub-menu");
-// }
-
-// let aboutUsArrow = document.querySelector(".about-us-arrow");
-// aboutUsArrow.onclick = function() {
-//  navLinks.classList.toggle("show-about-us-sub-menu");
-// }
-// let resourcesArrow = document.querySelector(".sub-menu-arrow");
-// resourcesArrow.onclick = function() {
-//     resourcesArrow.classList.toggle('transform-180');
-//     const subMenu = resourcesArrow.parentNode.querySelector('.sub-menu');
-//     subMenu.classList.toggle('display-sub-menu');    
-// }
-
+// Sidebar submenu open close js code for mobile menu
 const subMenuArrows = document.querySelectorAll('.sub-menu-arrow');
 subMenuArrows.forEach(subMenuArrow => {
     subMenuArrow.onclick = () => {
@@ -43,6 +22,7 @@ subMenuArrows.forEach(subMenuArrow => {
     }
 })
 
+// Sidebar sub-sub-menu open close js code for mobile menu
 const subSubMenuArrows = document.querySelectorAll('.sub-sub-menu-arrow');
 subSubMenuArrows.forEach(subSubMenuArrow => {
     subSubMenuArrow.onclick = () => {
